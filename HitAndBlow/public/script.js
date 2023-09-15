4420f725fb439c2ad1c7b3a15fde3fcf9eae1566
geonwoo218 <ekw218@naver.com>
2023-09-15 13:33:48 +0900

    0915 hitandblow
    포커스 설정
    엔터 설정

--- HitAndBlow/public/script.js
+++ HitAndBlow/public/script.js
@@ -1,3 +1,4 @@
+
 const content = document.getElementById('content');
 const startBtn = document.getElementById('start');
 const startBg = document.getElementById('startBg');
@@ -27,90 +28,130 @@ function gameRestart() { // 다시시작
 }
 //색깔 넣기
 const colors = ['blue', 'red', 'green', 'yellow', 'pink', 'gray'];
+const sets = document.querySelectorAll('.set');
 
 let ColorIndex = 0; // 선택된 색상 인덱스
 let selectedPosition = 1; // 선택된 위치 (1-1)
-
-// 1-1에 포커스를 초기 설정
-const FocusElement = document.querySelector('.box:nth-child(1)');
-FocusElement.focus();
-FocusElement.textContent = "☜\u00A0\u00A0\u00A0☞";
-let previousElement = FocusElement;
+let SetPosition = 0; // 첫번째 set 
 
 // 키보드 이벤트 처리
 document.addEventListener('keydown', (event) => {
-    previousElement.textContent = '';
+    let selectedSet = sets[SetPosition]; // 현재 선택된 set
 
     switch (event.key) {
-        case 'ArrowLeft': // 왼쪽 화살표 키를 누를 때, 이전 색상 선택
+        case 'ArrowLeft': // 이전 색상 선택
             ColorIndex = (ColorIndex - 1 + colors.length) % colors.length;
             break;
-        case 'ArrowRight': // 오른쪽 화살표 키를 누를 때, 다음 색상 선택
+        case 'ArrowRight': // 다음 색상 선택
             ColorIndex = (ColorIndex + 1) % colors.length;
             break;
-        case 'ArrowUp':// 위 화살표 키를 누를 때, 위치를 위로 이동
+        case 'ArrowUp': // 위로 이동
             if (selectedPosition > 1) {
                 selectedPosition--;
             }
             break;
-        case 'ArrowDown':// 아래 화살표 키를 누를 때, 위치를 아래로 이동
+        case 'ArrowDown': // 아래로 이동
             if (selectedPosition < 4) {
                 selectedPosition++;
             }
             break;
+        case 'Enter': // 다음 위치로 이동
+        if (SetPosition < sets.length-2) {
+            // 현재 set의 선택된 박스에서 엔터를 누르면 SetPosition을 증가시킴
+            SetPosition++; // 다음 set
+            selectedPosition = 1; // 첫 번째 박스로 이동
+            selectedSet = sets[SetPosition]; // 새로운 set 선택
+        }
+  
+        HitandBlow();
     }
 
     // 선택된 위치를 화면에 표시하고 포커스를 업데이트
-    const selectedElement = document.querySelector(`.box:nth-child(${selectedPosition})`);
+    const selectedElement = selectedSet.querySelector(`.box:nth-child(${selectedPosition})`);
     selectedElement.focus();
-    selectedElement.textContent = "☜\u00A0\u00A0\u00A0☞";
-    previousElement = selectedElement;
+    // 현재 set 내의 모든 박스에서 텍스트 제거
+    selectedSet.querySelectorAll('.box').forEach(box => {
+        box.textContent = '';
+    });
 
-});
+    // 첫 번째 박스에 ☜\u00A0\u00A0\u00A0☞ 표시
+    if (selectedPosition === 1) {
+        selectedElement.textContent = "☜\u00A0\u00A0\u00A0☞";
+    }
 
-document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-        case 'ArrowLeft':
-        case 'ArrowRight':
-            // 왼쪽 또는 오른쪽 화살표 키를 누를 때, 배경색 변경
-            const selectedElement = document.querySelector(`.box:nth-child(${selectedPosition})`);
-            selectedElement.style.backgroundColor = colors[ColorIndex];
-            break;
+    // 왼쪽 또는 오른쪽 화살표 키를 누를 때, 배경색 변경
+    if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
+        selectedElement.style.backgroundColor = colors[ColorIndex];
     }
 });
 
 //정답 랜덤색넣기
-function getRanddomColor(){
+function getRanddomColor() {
     const randColorIndex = [];
-    while(randColorIndex.length < 4){
-        const randIndex = Math.floor(Math.random()*6);
-        if(!randColorIndex.includes(randIndex)){
+
+    while (randColorIndex.length < 4) {
+        const randIndex = Math.floor(Math.random() * 6);
+        if (!randColorIndex.includes(randIndex)) {
             randColorIndex.push(randIndex);
         }
     }
-    return randColorIndex.map((ColorIndex)=> colors[ColorIndex]);
+    return randColorIndex.map((ColorIndex) => colors[ColorIndex]);
 }
+const resultColor = getRanddomColor(); //결과 색
 
-function addResultBox(){
+function addResultBox() {
     const resultBox = document.querySelector('.resultbox');
-    const Cindex = getRanddomColor();
     const resultBoxes = resultBox.querySelectorAll('.box');
-    console.log(Cindex);
 
-    Cindex.forEach((color,index)=>{
+    resultColor.forEach((color, index) => {
         resultBoxes[index].style.backgroundColor = color;
     });
 }
 
 //hit and blow 처리
-function HitandBlow(){
-    const colorContainer = document.querySelector('.colorContainer');
-    const colorbox = colorContainer.querySelectorAll('.box');
+function HitandBlow() {
+    let selectset = sets[SetPosition-1]; // 현재 선택된 set
+    const colorContainer = selectset.querySelector('.colorContainer');
+    const colorboxes = colorContainer.querySelectorAll('.box');
+    let userColor = [];
 
-    if(colorbox.backgroundColor !== '#dcdcdc'){
-        //resultbox랑 색 비교
+    colorboxes.forEach((box) => { //사용자 색상 배열
+        const backgroundColor = box.style.backgroundColor;
+        if (backgroundColor) {
+            userColor.push(backgroundColor);
+        }
+    });
+
+console.log("사용자 "+userColor);
+console.log('결과 '+resultColor);
+
+    let hit = 0, blow = 0;
+    const checkColor = userColor.filter((i, index, self) => self.indexOf(i) === index); //사용자 중복색제거
+
+    for (let i = 0; i < 4; i++) {
+        if (checkColor[i] === resultColor[i]) {
+            hit++;
+        } else if (resultColor.includes(checkColor[i]) && checkColor.indexOf(checkColor[i]) !== i) {
+            blow++;
+        }
+    }
+    console.log("hit "+hit);
+    console.log("blow "+blow);
+    //hb 색추가
+    const hbContainer = document.querySelector('.hbContainer');
+    const hbbox = hbContainer.querySelectorAll('.hbbox');
+    let index = 0;
+    for (let i = 0; i < hit; i++) {
+        hbbox[index++].style.backgroundColor = 'red';
+    }
+    for (let i = 0; i < blow; i++) {
+        hbbox[index++].style.backgroundColor = "yellow";
+    }
+    if (hit === 4) {
+        alert("게임 승리");
     }
 }
+
 
 
 
