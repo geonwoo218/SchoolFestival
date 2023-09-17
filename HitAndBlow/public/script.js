c4254b041b5c83d2fab76d5d0e845321d2f3fa00
geonwoo218 <ekw218@naver.com>
2023-09-17 17:24:32 +0900

    0917 hitandblow
    css수정

--- HitAndBlow/public/script.js
+++ HitAndBlow/public/script.js
@@ -6,7 +6,6 @@ const colorList = document.getElementById('colorList');
 const header = document.querySelector('header');
 const htpbg = document.getElementById('htpbg');
 
-
 startBtn.addEventListener('click', GameStart);
 
 function GameStart() { //게임시작
@@ -33,6 +32,7 @@ const sets = document.querySelectorAll('.set');
 let ColorIndex = 0; // 선택된 색상 인덱스
 let selectedPosition = 1; // 선택된 위치 (1-1)
 let SetPosition = 0; // 첫번째 set 
+let previous; //이전 위치
 
 // 키보드 이벤트 처리
 document.addEventListener('keydown', (event) => {
@@ -48,31 +48,35 @@ document.addEventListener('keydown', (event) => {
         case 'ArrowUp': // 위로 이동
             if (selectedPosition > 1) {
                 selectedPosition--;
+                previous.textContent="";
             }
             break;
         case 'ArrowDown': // 아래로 이동
             if (selectedPosition < 4) {
                 selectedPosition++;
+                previous.textContent = "";
             }
             break;
         case 'Enter': // 다음 위치로 이동
-        if (SetPosition < sets.length-2) {
+        if (SetPosition < sets.length-1) {
             // 현재 set의 선택된 박스에서 엔터를 누르면 SetPosition을 증가시킴
             SetPosition++; // 다음 set
             selectedPosition = 1; // 첫 번째 박스로 이동
             selectedSet = sets[SetPosition]; // 새로운 set 선택
+            previous.textContent = "";
+            HitandBlow();
+        }else if(SetPosition === sets.length){
+            HitandBlow();
         }
   
-        HitandBlow();
+        
     }
 
     // 선택된 위치를 화면에 표시하고 포커스를 업데이트
     const selectedElement = selectedSet.querySelector(`.box:nth-child(${selectedPosition})`);
+    
     selectedElement.focus();
-    // 현재 set 내의 모든 박스에서 텍스트 제거
-    selectedSet.querySelectorAll('.box').forEach(box => {
-        box.textContent = '';
-    });
+    selectedElement.textContent = "☜\u00A0\u00A0\u00A0☞";
 
     // 첫 번째 박스에 ☜\u00A0\u00A0\u00A0☞ 표시
     if (selectedPosition === 1) {
@@ -83,6 +87,7 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowLeft' || event.key === 'ArrowRight') {
         selectedElement.style.backgroundColor = colors[ColorIndex];
     }
+    previous = selectedElement;
 });
 
 //정답 랜덤색넣기
@@ -109,6 +114,7 @@ function addResultBox() {
 }
 
 //hit and blow 처리
+
 function HitandBlow() {
     let selectset = sets[SetPosition-1]; // 현재 선택된 set
     const colorContainer = selectset.querySelector('.colorContainer');
@@ -122,8 +128,8 @@ function HitandBlow() {
         }
     });
 
-console.log("사용자 "+userColor);
-console.log('결과 '+resultColor);
+    console.log("사용자 "+userColor);
+    console.log('결과 '+resultColor);
 
     let hit = 0, blow = 0;
     const checkColor = userColor.filter((i, index, self) => self.indexOf(i) === index); //사용자 중복색제거
@@ -131,14 +137,14 @@ console.log('결과 '+resultColor);
     for (let i = 0; i < 4; i++) {
         if (checkColor[i] === resultColor[i]) {
             hit++;
-        } else if (resultColor.includes(checkColor[i]) && checkColor.indexOf(checkColor[i]) !== i) {
+        } else if (resultColor.includes(checkColor[i])) {
             blow++;
         }
     }
     console.log("hit "+hit);
     console.log("blow "+blow);
     //hb 색추가
-    const hbContainer = document.querySelector('.hbContainer');
+    const hbContainer = selectset.querySelector('.hbContainer');
     const hbbox = hbContainer.querySelectorAll('.hbbox');
     let index = 0;
     for (let i = 0; i < hit; i++) {
@@ -147,11 +153,27 @@ console.log('결과 '+resultColor);
     for (let i = 0; i < blow; i++) {
         hbbox[index++].style.backgroundColor = "yellow";
     }
-    if (hit === 4) {
-        alert("게임 승리");
+    console.log("setposition "+SetPosition);
+    console.log("sets.length "+sets.length);
+    
+    
+    console.log("============================================");
+
+}
+
+function gameCheck(hit){
+    if(hit === 4){
+        gameWin();
+    }else{
+        gameOver();
     }
 }
 
+function gameWin(){
+    alert("win");
+}
 
-
+function gameOver(){
+    alert("over");
+}
 
