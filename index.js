4c7c6a3cd7b897ca48deb6e35c3c01913ebbd96b
geonwoo218 <ekw218@naver.com>
2023-09-26 22:55:50 +0900

    0926
    메인페이지 수정
    게임 설명문들 추가
    제목 추가

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const infobar = document.querySelectorAll('.infobar');
 checkbox.addEventListener("change", function () {
     if (checkbox.checked) {
         infobar.forEach(function (i) {
-            setTimeout(() => {
-                i.style.display = 'block';
-                t++;
-            }, 1000);
-
+            i.style.display = 'block';
+            setTimeout(function () {
+                i.style.opacity = '1';
+            }, 50);
         });
     } else {
-
-        i.style.display = 'none`';
-
+        infobar.forEach(function (i) {
+            i.style.opacity = '0';
+            setTimeout(function () {
+                i.style.display = 'none';
+            }, 500);
+        });
     }
 });
